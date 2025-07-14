e8bd9686fcc550d9f751bb8f9cfa2435606aa387
이연우 <yeunwoo217@gmail.com>
2025-07-14 14:06:36 +0900

    Level 1 - 2 ) 사칙 연산 기호를 적합한 타입으로 선언한 변수에 저장

--- nbcamp-java-personal-task-new/nbcamp-java-personal-task/src/main/java/level1/calculator/App.java
+++ nbcamp-java-personal-task-new/nbcamp-java-personal-task/src/main/java/level1/calculator/App.java
@@ -23,6 +23,10 @@ public class App {
             System.out.println("음수는 입력할 수 없습니다.");
         }
 
-        System.out.println("num1 = " + num1 + ", num2 = " + num2);
+        System.out.print("사칙 연산 기호를 입력하세요: "); // +, -, *, /
+        char operator = sc.next().charAt(0); // 사칙 연산 기호를 적합한 타입으로 선언한 변수에 저장
+        // charAt(0): 문자열의 0번째 위치에 있는 문자(첫 글자) 반환
+
+        System.out.println("입력된 기호: " + operator);
     }
 }
