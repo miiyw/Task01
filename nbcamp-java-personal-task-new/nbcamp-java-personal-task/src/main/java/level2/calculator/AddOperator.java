f7660ace76ee5d210ca4b8c0b6bd540b6c939d30
이연우 <yeunwoo217@gmail.com>
2025-07-14 15:31:46 +0900

    Level 2 - 9 ) ArithmeticCalculator 클래스의 사칙 연산 메서드 책임 분리

---
+++ nbcamp-java-personal-task-new/nbcamp-java-personal-task/src/main/java/level2/calculator/AddOperator.java
@@ -0,0 +1,7 @@
+package level2.calculator;
+
+public class AddOperator {
+    public double operate(double num1, double num2) {
+        return num1 + num2;
+    }
+}
---
+++ nbcamp-java-personal-task-new/nbcamp-java-personal-task/src/main/java/level2/calculator/MultiplyOperator.java
@@ -0,0 +1,7 @@
+package level2.calculator;
+
+public class MultiplyOperator {
+    public  double operate(double num1, double num2) {
+        return num1 * num2;
+    }
+}
